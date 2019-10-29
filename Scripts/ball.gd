f4dcf41b8c2a20781b91df0e79d08f386b8786d6
DandyDave <54556179+DandyDave@users.noreply.github.com>
2019-10-29 17:41:10 -0400

    finished

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -6,20 +6,21 @@ signal lives
 signal score
 
 func _ready():
-	contact_monitor = true
-	set_max_contacts_reported(4)
-	var WorldNode = get_node("/root/World")
-	connect("score", WorldNode, "increase_score")
-	connect("lives", WorldNode, "decrease_lives")
+ contact_monitor = true
+ set_max_contacts_reported(4)
+ var WorldNode = get_node("/root/World")
+ connect("score", WorldNode, "increase_score")
+ connect("lives", WorldNode, "decrease_lives")
 
 func _physics_process(delta):
-    var bodies = get_colliding_bodies()
-    for body in bodies:
-        if body.is_in_group("Tiles"):
-			emit_signal("score",body.score)
-            body.queue_free()
-    
-    if position.y > get_viewport_rect().end.y:
-        queue_free()
-		emit_signal("lives"))
-        print("Died")
+ var bodies = get_colliding_bodies()
+ for body in bodies:
+  if body.is_in_group("Tiles"):
+   emit_signal("score",body.score)
+   body.queue_free()
+  if body.get_name() == "Paddle":
+   pass
+  
+ if position.y > get_viewport_rect().end.y:
+  emit_signal("lives")
+  queue_free()
